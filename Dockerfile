0893245af43b2028a0110812ac73c88b88ad504a
zlqu <zlqu@trip.com>
2023-12-26 21:09:56 +0800

    feat: docker

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+FROM rust:1.74.0 as builder
+
+RUN USER=root cargo new --bin company-api
+WORKDIR ./company-api
+COPY ./Cargo.toml ./Cargo.toml
+RUN cargo build --release
+
+
+# Build web app with own code
+RUN rm src/*.rs
+ADD . ./
+# RUN rm ./target/release/deps/company-api*
+RUN cargo build --release
+
+
+FROM debian:12.4
+ARG APP=/usr/src/app
+
+
+EXPOSE 3002
+
+ENV TZ=Etc/UTC \
+    APP_USER=appuser
+
+RUN groupadd $APP_USER \
+    && useradd -g $APP_USER $APP_USER \
+    && mkdir -p ${APP}
+
+COPY --from=builder /company-api/target/release/company-api ${APP}/company-api
+
+RUN chown -R $APP_USER:$APP_USER ${APP}
+
+USER $APP_USER
+WORKDIR ${APP}
+
+CMD ["./company-api"]
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
 use std::net::SocketAddr;
 mod models;
 mod routes;
-use axum::{Router, Server};
+use axum::{response::Html, routing::get, Router, Server};
 use mongodb::Client;
+
+async fn handle_test() -> Html<String> {
+    println!("test");
+    Html("<div>test</div>".to_string())
+}
 
 #[tokio::main]
 async fn main() {
+    println!("start");
     let client: Client = Client::with_uri_str("mongodb://localhost:27017")
         .await
         .unwrap();
     let app: Router<Client> = Router::new()
         .merge(routes::types::create_route())
-        .merge(routes::productions::create_route());
+        .merge(routes::productions::create_route())
+        .route("/test", get(handle_test));
     let app: Router = app.with_state(client);
 
-    let addr: SocketAddr = SocketAddr::from(([127, 0, 0, 1], 3001));
+    let addr: SocketAddr = SocketAddr::from(([0, 0, 0, 0], 3002));
 
+    println!("start server");
     Server::bind(&addr)
         .serve(app.into_make_service())
         .await
